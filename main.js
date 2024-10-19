1b156b7c3c7272f0cceec7efa7b65ec120039430
frost3dWave <181492974+frost3dWave@users.noreply.github.com>
2024-10-19 14:13:05 +0530

    created a function for column & row grid creation.
    made column & row grid creation to be as part of function.
    made the grid to update dynamically when interacting with the slider input.

--- main.js
+++ main.js
@@ -2,27 +2,28 @@ const gridContainer = document.querySelector(".grid_container");
 const sliderValue = document.querySelector(".slider_value");
 const slider = document.querySelector(".slider");
 
-sliderValue.textContent = `${slider.value}x${slider.value}`;
+let gridSize = slider.value;
+sliderValue.textContent = `${gridSize}x${gridSize}`;
+
+createGrid(gridSize); //Initial grid creation
 
 slider.addEventListener("input", () => {
-    sliderValue.textContent = `${slider.value}x${slider.value}`;
+    gridSize = slider.value;
+    sliderValue.textContent = `${gridSize}x${gridSize}`;
+    createGrid(gridSize); 
 });
-// console.log(slider.value);
 
-// square grids column creation
-for (i=0; i<slider.value; i++){
-    const squareGrids = document.createElement("div");
-    squareGrids.classList.add("squareGrids");
-    squareGrids.style.setProperty("width", `calc(37.5rem / ${slider.value})`);
-    squareGrids.style.setProperty("height", `calc(37.5rem / ${slider.value})`);
-    gridContainer.appendChild(squareGrids);
+// function to create the grid
+function createGrid(gridSize){
+    gridContainer.innerHTML = ""; // clears the existing grid
+
+    for(let i = 0; i < gridSize; i++){ // for creating the column
+        for(let j = 0; j < gridSize; j++){ // for creating the rows
+            const squareGrids = document.createElement("div");
+            squareGrids.classList.add("squareGrids");
+            squareGrids.style.setProperty("width", `calc(100% / ${gridSize})`);
+            squareGrids.style.setProperty("height", `calc(100% / ${gridSize})`);
+            gridContainer.appendChild(squareGrids);
+        }
+    }
 }
-
-// square grids row creation
-// for (j=1; j<slider.value; i++){
-//     const rowGrids = document.createElement("div");
-//     rowGrids.classList.add("rowGrids");
-//     rowGrids.style.setProperty("width", `calc(37.5rem / ${slider.value})`);
-//     rowGrids.style.setProperty("height", `calc(37.5rem / ${slider.value})`);
-//     squareGrids.appendChild(rowGrids);
-// }
