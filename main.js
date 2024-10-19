29ec44f3bb7ddb6a042e51a4b96546f0b275d0a6
frost3dWave <181492974+frost3dWave@users.noreply.github.com>
2024-10-19 15:58:01 +0530

    updated to paint grid when clicking & moving over updated to paint when clicking & moving over the grid with current selected color. also made it so user can change color, & paint with new color over the grid.

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const gridContainer = document.querySelector(".grid_container");
+const colorPallete = document.querySelector(".color_wheel");
 const sliderValue = document.querySelector(".slider_value");
 const slider = document.querySelector(".slider");
 
 let gridSize = slider.value;
 sliderValue.textContent = `${gridSize}x${gridSize}`;
+let currentColor = colorPallete.value;
 
 createGrid(gridSize); //Initial grid creation
 
@@ -13,6 +15,7 @@ slider.addEventListener("input", () => {
     createGrid(gridSize); 
 });
 
+let isDrawing = false;
 // function to create the grid
 function createGrid(gridSize){
     gridContainer.innerHTML = ""; // clears the existing grid
@@ -23,7 +26,19 @@ function createGrid(gridSize){
             squareGrids.classList.add("squareGrids");
             squareGrids.style.setProperty("width", `calc(100% / ${gridSize})`);
             squareGrids.style.setProperty("height", `calc(100% / ${gridSize})`);
+
+            squareGrids.addEventListener("mousedown", () => isDrawing = true);
+            squareGrids.addEventListener("mousemove", paint);
+            squareGrids.addEventListener("mouseup", () => isDrawing = false);
             gridContainer.appendChild(squareGrids);
         }
+        gridContainer.addEventListener("mouseleave", () => isDrawing = false);
+    }
+}
+
+function paint(e){
+    currentColor = colorPallete.value;
+    if (isDrawing){
+        e.target.style.backgroundColor = `${currentColor}`;
     }
 }
